7ea0b1e0ffef77327146ead7d15dc8010a80b3a2
Ludvig Kindberg <lki@kth.se>
2019-10-30 16:28:24 +0100

    Updates

---
+++ precache-manifest.ecb42dae62dc409b957768c04c43e383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22c6d7bb62d869100d1b7aa0d4f1fb18",
+    "url": "/timegame/index.html"
+  },
+  {
+    "revision": "2532d05944eb9dd55855",
+    "url": "/timegame/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3734807303d908fcc12d",
+    "url": "/timegame/static/js/2.66cfd510.chunk.js"
+  },
+  {
+    "revision": "2532d05944eb9dd55855",
+    "url": "/timegame/static/js/main.a3e7ea9b.chunk.js"
+  },
+  {
+    "revision": "9940d4241d894a40a114",
+    "url": "/timegame/static/js/runtime-main.b9e1e91f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/timegame/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timegame/timepage/precache-manifest.c44237fa3dae5c0a33debfa8bf36e78a.js"
+  "/timegame/precache-manifest.ecb42dae62dc409b957768c04c43e383.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timegame/timepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timegame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
