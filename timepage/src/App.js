bd7631219caff6322b7bfb1b42fcac2ca7563046
Ludvig Kindberg <lki@kth.se>
2019-10-30 16:17:21 +0100

    ADD: init commit for the webpage

---
+++ timepage/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          The inital skeleton webpage for the Time game in the course DH2650.
+          Placeholder text for now.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
