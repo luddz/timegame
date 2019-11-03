db79fbfc16c4ab9f5ad49a1c0a58d90441d2eff7
Ludvig Kindberg <lki@kth.se>
2019-11-03 11:31:37 +0100

    ADD: about files

---
+++ timepage/src/About/about.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './about.css'
+import '../App.css'
+
+class About extends Component {
+  render(){
+    return (
+      <div className="App">
+        <h2> About PLACEHOLDER NAME </h2>
+        <p>  Here we should write about the game. </p>
+        <h2> The team behind the game </h2>
+        <p> Here we should write what each one of us did, and what we learnt maybe </p>
+      </div>
+    );
+  }
+}
+
+export default About;
