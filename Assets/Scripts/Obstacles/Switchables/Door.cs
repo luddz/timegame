37cf19e5056cd9498d3b93bab5a47e05c68e5d8e
bhatnathan <bhatnathan@gmail.com>
2019-11-05 16:14:51 +0100

    Added doors and buttons
    Also added code for these and a general switch/switchable system. Sorry for large commit.

---
+++ Assets/Scripts/Obstacles/Switchables/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : SwitchableSystem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Open door
+    protected override void SwitchOn() {
+        gameObject.SetActive(false); //TODO add animations and SFX
+    }
+
+    // Close door
+    protected override void SwitchOff() {
+        gameObject.SetActive(true); //TODO add animations and SFX
+    }
+}
---
+++ Assets/Scripts/Obstacles/Switchables/SwitchableSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * An abstract class that all game objects that can be switched on or off should inherit.
+ * Examples are doors or lasers. This does not include the actual switch itself.
+ */
+public abstract class SwitchableSystem : MonoBehaviour
+{
+    [SerializeField]
+    protected bool isOn;
+
+    //Switches the switchable to on if it is off or off if it is on.
+    public void Switch() {
+        isOn = !isOn;
+        if (isOn)
+            SwitchOn();
+        else
+            SwitchOff();
+    }
+
+    //Switches the switchable to switchTo
+    public void SwitchTo(bool switchTo) {
+        if (isOn == switchTo) //Do nothing if it is switching to what it already is.
+            return;
+
+        isOn = switchTo;
+        if (isOn)
+            SwitchOn();
+        else
+            SwitchOff();
+    }
+
+    //What happens when we turn on the switch?
+    protected abstract void SwitchOn();
+
+    //What happens when we turn off the switch?
+    protected abstract void SwitchOff();
+}
