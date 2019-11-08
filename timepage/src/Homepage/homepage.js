914e1cf0ee5e7cefd1c5b5e6c2991c10946e9c0e
Ludvig Kindberg <lki@kth.se>
2019-11-08 09:55:37 +0100

    CHG: file structure a bit

---
+++ timepage/src/Homepage/homepage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './homepage.css'
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>
+            Asters Planetai (Time game)
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Homepage
