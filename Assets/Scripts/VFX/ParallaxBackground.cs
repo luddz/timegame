558e89a1628f7633de25dd1fc17e2665db6058fe
bhatnathan <bhatnathan@gmail.com>
2019-11-14 20:17:12 +0100

    Parallax background

---
+++ Assets/Scripts/VFX/ParallaxBackground.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private GameObject camera;
+    [SerializeField] private float parallaxAmount;
+
+    private float length;
+    private float startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float temp = camera.transform.position.x * (1 - parallaxAmount);
+        float distance = camera.transform.position.x * parallaxAmount;
+
+        transform.position = new Vector3(startPosition + distance, camera.transform.position.y, transform.position.z);
+
+        if (temp > startPosition + length)
+            startPosition += length;
+        else if (temp < startPosition - length)
+            startPosition -= length;
+    }
+}
