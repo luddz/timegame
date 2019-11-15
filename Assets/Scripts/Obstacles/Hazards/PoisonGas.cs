a871f339e69c8aa3a558731e04ac478c150cddc4
Alvin Hager <alvinhager@n153-p39.eduroam.kth.se>
2019-11-15 12:05:57 +0100

    add simple poisonGas

---
+++ Assets/Scripts/Obstacles/Hazards/PoisonGas.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** poison gas kills the player when the gas collides with the player */
+public class PoisonGas : MonoBehaviour
+{
+
+    /**
+     * Character has hit the poisongas and dying
+     */
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            other.gameObject.GetComponent<CharacterMovement>().Die();
+        }
+    }
+
+}
