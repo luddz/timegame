6df672020bef8b22cb2854913f2bda45c052168b
Ludvig Kindberg <lki@kth.se>
2019-11-25 10:50:22 +0100

    Added solidify sound for the player only

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -61,6 +61,7 @@ public class CharacterMovement : MonoBehaviour
             }
         } else if (horizontalV != 0) { //Walk
             body.velocity = new Vector2(horizontalV, body.velocity.y);
+            
         }
 
         //Set facing variable
@@ -95,6 +96,8 @@ public class CharacterMovement : MonoBehaviour
         if (controller.SolidifyButtonDown() && analyser.IsGroundDown() && !analyser.IsCharacterOverlapping()) {
             gameObject.layer = 8;
             body.constraints = RigidbodyConstraints2D.FreezeAll;
+            if (PlayerManager.Instance.IsPlayer(gameObject))
+                AudioManager.Instance.Play("Solidify");
         }
 
         //Clearing Checkpoint
