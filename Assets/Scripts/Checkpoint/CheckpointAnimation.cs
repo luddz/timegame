9323a36efe18e8fb25f58c865ec120e4b009cf67
Alvin Hager <alvinhager@Alvins-Air.lan>
2019-11-29 14:44:44 +0100

    added working animations, need to be attached to checkpoints

---
+++ Assets/Scripts/Checkpoint/CheckpointAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointAnimation : MonoBehaviour
+{
+
+    //Componenets
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+       
+    }
+
+    public void setActive() {
+        anim.SetBool("isActive", true);
+        Debug.Log("ACtive");
+    }
+
+    public void setInactive() {
+        anim.SetBool("isActive", false);
+        Debug.Log("inACtive");
+    }
+
+
+    
+}
