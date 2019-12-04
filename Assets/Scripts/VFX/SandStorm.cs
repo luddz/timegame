949aa0651c895c51af6a7086c9e60082e70c468e
mangoglass <tom.axblad@gmail.com>
2019-12-04 22:03:17 +0100

    Added a sandstorm prefab to be used on the top level

---
+++ Assets/Scripts/VFX/SandStorm.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandStorm : MonoBehaviour {
+
+    private ParticleSystem particleSystem;
+    private ParticleSystem secondaryParticleSystem;
+
+    void Awake() {
+        particleSystem = GetComponent<ParticleSystem>();
+        secondaryParticleSystem = GetComponentInChildren<ParticleSystem>();
+    }
+
+    public void StopStorm() {
+        particleSystem.Stop();
+        secondaryParticleSystem.Stop();
+    }
+
+    public void StartStorm() {
+        particleSystem.Play();
+        secondaryParticleSystem.Play();
+    }
+}
